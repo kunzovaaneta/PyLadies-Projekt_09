efad94f8e785ac84eec8cb5af5cf525cda54a59b
Aneta Kunzováns <kunzovaaneta@seznam.cz>
2017-11-28 22:18:42 +0100

    revize-pokus

--- DU_9_0.py
+++ DU_9_0.py
@@ -1,13 +1,11 @@
-def vytvareni_a_vraceni_slovniku():
+def vytvareni_slovniku_mocnin(zadane_cislo):
 	"""Pro zadané číslo n vytvoří a vrátí slovník, kde jako klíče budou čísla od jedné do n a
 	jako hodnoty k nim jejich druhé mocnin"""
 
-	zadane_cislo = int(input("zadej číslo větší než 1 -"))
-	jednotlivé_argumenty_slovniku = []
+	slovnik_mocnin={}
 	for cislo in range (1,zadane_cislo+1):
-		jednotlivé_argumenty_slovniku = [(cislo, cislo**2)] + jednotlivé_argumenty_slovniku
+		vytvareni_slovniku_mocnin[cislo] = cislo**2
+	
+	return slovnik_mocnin
 
-	slovnik_argumentu = dict(jednotlivé_argumenty_slovniku)
-	print(slovnik_argumentu)
-
-vytvareni_a_vraceni_slovniku()
+print (vytvareni_slovniku_mocnin(3))
